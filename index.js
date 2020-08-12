7295d29755a7ed9c20828c592438b561d91e09c4
ami1708 <amisha17200@gmail.com>
2020-08-12 19:11:21 +0530

    added router and controller to our server

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express =  require('express')
 const app = express();
 const port = 4000;
+ 
 
+app.use('/',require('./routes'))
 app.listen(port,function(err){
     if(err){
         console.log('Error:',err);
