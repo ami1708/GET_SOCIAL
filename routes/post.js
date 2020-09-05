eec279f421ace6dd8b469c6d67582e510991003d
ami1708 <amisha17200@gmail.com>
2020-09-06 02:18:44 +0530

    add

--- routes/post.js
+++ routes/post.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router();
+const passport = require("passport");
 
 
 // tell the router tht we are adding the controller function
 const postController = require('../controllers/post_controller')
 // to get the userController functions in our code
-router.post('/create',postController.create)
+router.post('/create',passport.checkAuthentication, postController.create)
 module.exports = router;
 
 
