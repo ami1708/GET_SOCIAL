8a4b0fe9205a3895e347553085d4c82afa267826
ami1708 <amisha17200@gmail.com>
2020-09-21 21:03:31 +0530

    add

--- index.js
+++ index.js
@@ -1,95 +1,87 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cookieParser = require ('cookie-parser')
+const cookieParser = require("cookie-parser");
 const port = 2000;
 
 // setting up our layouts
-const expressLayouts = require('express-ejs-layouts');
-const db = require('./config/mongoose');
+const expressLayouts = require("express-ejs-layouts");
+const db = require("./config/mongoose");
 
 //used for session cookie
-const session = require('express-session');
-const passport =require('passport')
-const passportLocal =require('./config/passport-local-strategy')
-const MongoStore = require('connect-mongo')(session);
-const sassMiddleware = require('node-sass-middleware')
+const session = require("express-session");
+const passport = require("passport");
+const passportLocal = require("./config/passport-local-strategy");
+const MongoStore = require("connect-mongo")(session);
+const sassMiddleware = require("node-sass-middleware");
 //const { urlencoded } = require('express');
-const flash = require('connect-flash');
-const customMiddleware = require('./config/middleware');
+const flash = require("connect-flash");
+const customMiddleware = require("./config/middleware");
 
-app.use(sassMiddleware({
-    src: './assets/scss',
-    dest:'./assets/css',
+app.use(
+  sassMiddleware({
+    src: "./assets/scss",
+    dest: "./assets/css",
     //do i want to display some errors the file which cant be compiled
-    debug :true,
-    outputStyle : 'extended',
+    debug: true,
+    outputStyle: "extended",
     //where should my server look for css file
-    prefix : './css'
-
-}))
-
+    prefix: "./css",
+  })
+);
 
 //setting up the middleware
-app.use(express.urlencoded())
-
-
+app.use(express.urlencoded());
 
 app.use(cookieParser());
 
-// using the static files 
-app.use(express.static('./assets'));
+// using the static files
+app.use(express.static("./assets"));
 // using the layout
 app.use(expressLayouts);
 // extract styles and scripts from sub pages to your layouts
-app.set('layout extractStyles',true);
-app.set('layout extractScripts',true);
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
 // use express router
 
 //mongo store is used to store the session cookie in the db
 // set up the view engine
-app.set('view engine', 'ejs');
-app.set('views', './views');
+app.set("view engine", "ejs");
+app.set("views", "./views");
 //add a middleware which takes the session cookie and encrypts it
-app.use(session({
-    name : "codial",
+app.use(
+  session({
+    name: "codial",
     //encrypted
     //to do change the secret before deployment to production mode that is  sending to server
     secret: "something",
-    saveUninitialized:false,
+    saveUninitialized: false,
     cookie: {
-        maxAge: (1000*60*100)
+      maxAge: 1000 * 60 * 100,
     },
-    store : new MongoStore({
-        
-            mongooseConnection :db,
-            autoRemove : 'disabled'
-        
+    store: new MongoStore({
+      mongooseConnection: db,
+      autoRemove: "disabled",
     }),
     function(err) {
-        console.log(err || 'connect-mongo setup ok')
-        
-    }
-}))
+      console.log(err || "connect-mongo setup ok");
+    },
+  })
+);
 
 app.use(passport.initialize());
 
 app.use(passport.session());
 app.use(passport.setAuthenticatedUser);
 
+app.use(flash()); //flash messages are set up to cookies which stores the session information
+app.use(customMiddleware.setFlash);
+app.use(passport.isUserAuth);
+app.use("/", require("./routes"));
 
+app.listen(port, function (err) {
+  if (err) {
+    console.log(`Error in running the server: ${err}`);
+  }
 
-app.use(flash())//flash messages are set up to cookies which stores the session information
-app.use(customMiddleware.setFlash)
-app.use(passport.isUserAuth)
-app.use('/', require('./routes'));
-
-
-app.listen(port, function(err){
-    if (err){
-        console.log(`Error in running the server: ${err}`);
-    }
-
-    console.log(`Server is running on port: ${port}`);
+  console.log(`Server is running on port: ${port}`);
 });
-
-
--- routes/users.js
+++ routes/users.js
@@ -1,26 +1,34 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
-const passport = require('passport')
+const passport = require("passport");
 // tell the router tht we are adding the controller function
-const userController = require('../controllers/user_controller')
+const userController = require("../controllers/user_controller");
 // to get the userController functions in our code
 //we want the profile page to be accessible only when the user is signed in
-router.get('/profile/:id',passport.checkAuthentication, userController.profile)
-router.post('/update/:id',passport.checkAuthentication, userController.update)
+router.get(
+  "/profile/:id",
+  passport.checkAuthentication,
+  userController.profile
+);
+router.post("/update/:id", passport.checkAuthentication, userController.update);
 
-router.get('/sign-in',userController.signIn)
-router.get('/sign-up',userController.signUp)
+router.get("/sign-in", userController.signIn);
+router.get("/sign-up", userController.signUp);
 //u  called post js and comment js here?
 
-module.exports= router;
+module.exports = router;
 
-router.post('/create',userController.create)
+router.post("/create", userController.create);
 //use passport as a middleware to authenticate
 //passport first authenticates it
-router.post('/create-session',passport.authenticate(
-    'local',
-    {failureRedirect:'/users/sign-in'},//this is return when authentication fails
- ) ,userController.createSession);//this function is called if it is done (authentication)
+router.post(
+  "/create-session",
+  passport.authenticate(
+    "local",
+    { failureRedirect: "/users/sign-in" } //this is return when authentication fails
+  ),
+  userController.createSession
+); //this function is called if it is done (authentication)
 
- router.get('/sign-out',userController.destroySession)
+router.get("/sign-out", userController.destroySession);
 module.exports = router;
