0321b442ce4284f2843945b65c8296be3a5316b9
ami1708 <amisha17200@gmail.com>
2020-09-28 01:25:04 +0530

    add

--- .vscode/mailer/comments_mailer.js
+++ .vscode/mailer/comments_mailer.js
@@ -1,2 +1,16 @@
 //when i comment on  someone post the mail comes to me
-const nodemailer = require('nodemailer')
+const nodeMailer = require("../../config/nodemailer");
+// this is another way of exporting a methodxports
+//arrow function
+exports.newComment = (comment) => {
+  console.log("inside new Comment mailer");
+
+  nodeMailer.transporter.sendMail({
+      from : 'amisha17200@gmail.com',
+//when we are sending mail to user who has commented
+      to: comment.user.email,
+// post.user.comment.email
+      subject: 'new comment published',
+      html : '<h1> Yup now your comment is published!</h1>'
+  },err);
+};
