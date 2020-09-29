0b5fd698682130ff74ce9af18059dd0051bbbd3b
ami1708 <amisha17200@gmail.com>
2020-09-29 23:04:34 +0530

    add

--- .vscode/mailer/comments_mailer.js
+++ .vscode/mailer/comments_mailer.js
@@ -2,14 +2,17 @@ const nodeMailer = require("../../config/nodemailer");
 // this is another way of exporting a method
 exports.newComment = (comment) => {
   console.log("inside newComment mailer", comment);
-  let htmlString = nodeMailer.renderTemplate({comment:comment},'/comments/new_comment')
+  let htmlString = nodeMailer.renderTemplate(
+    { comment: comment },
+    "/comments/new_comment.ejs"
+  );
 
   nodeMailer.transporter.sendMail(
     {
       from: "amisha17200@gmail.com",
       to: comment.user.email,
       subject: "New Comment Published!",
-      html: htmlSring
+      html: htmlString,
     },
     (err, info) => {
       if (err) {
