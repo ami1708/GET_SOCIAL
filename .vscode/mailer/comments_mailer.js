ba52f807ae8d97c6c44356837305d54991278749
ami1708 <amisha17200@gmail.com>
2020-09-29 20:50:10 +0530

    add

--- .vscode/mailer/comments_mailer.js
+++ .vscode/mailer/comments_mailer.js
@@ -2,13 +2,14 @@ const nodeMailer = require("../../config/nodemailer");
 // this is another way of exporting a method
 exports.newComment = (comment) => {
   console.log("inside newComment mailer", comment);
+  let htmlString = nodeMailer.renderTemplate({comment:comment},'/comments/new_comment')
 
   nodeMailer.transporter.sendMail(
     {
-      from: "amisha17300@gmail.com",
+      from: "amisha17200@gmail.com",
       to: comment.user.email,
       subject: "New Comment Published!",
-      html: "<h1>Yup, your comment is now published!</h1>",
+      html: htmlSring
     },
     (err, info) => {
       if (err) {
--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,37 +1,41 @@
 const nodemailer = require("nodemailer");
-const ejs = require('ejs');
-const path = require('path')
+const ejs = require("ejs");
+const path = require("path");
 
 //part which sends the emails
 let transporter = nodemailer.createTransport({
-    service: 'gmail',
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: false,
-    auth: {
-        user: 'alchemy.cn18',
-        pass: 'codingninjas'
-    }
+  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: "amisha17200@gmail.com",
+    pass: "amiabhi1708",
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
 });
 
-
 let renderTemplate = (data, relativePath) => {
-    let mailHTML;
-    ejs.renderFile(
-        path.join(__dirname, '../views/mailers', relativePath),
-        data,
-        function(err, template){
-         if (err){console.log('error in rendering template'); return}
-         // if no error
-         mailHTML = template;
-        }
-    )
+  let mailHTML;
+  ejs.renderFile(
+    path.join(__dirname, "../views/mailers", relativePath),
+    data,
+    function (err, template) {
+      if (err) {
+        console.log("error in rendering template");
+        return;
+      }
+      // if no error
+      mailHTML = template;
+    }
+  );
 
-    return mailHTML;
-}
-
+  return mailHTML;
+};
 
 module.exports = {
-    transporter: transporter,
-    renderTemplate: renderTemplate
-}
+  transporter: transporter,
+  renderTemplate: renderTemplate,
+};
