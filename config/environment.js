e6f2585c3a1d6cfbe34ba2073a3f02c10ac3f14d
ami1708 <amisha17200@gmail.com>
2020-11-19 13:51:18 +0530

    add

--- config/environment.js
+++ config/environment.js
@@ -1,3 +1,15 @@
+const fs = require("fs");
+const rfs = require("rotating-file-stream");
+const path = require("path");
+
+const logDirectory = path.join(__dirname, "../production_logs");
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+
+const accessLogStream = rfs("access.log", {
+  interval: "1d",
+  path: logDirectory,
+});
+
 const development = {
   name: "development",
   asset_path: process.CODIAL_ASSET_PATH,
@@ -17,13 +29,15 @@ const development = {
     "741438108020-vm71e2n8nmi68nclqen0rtuq9ssvs9bq.apps.googleusercontent.com",
   google_client_secret: "9K6fjTc2XhxdY5JuRWW_dkru",
   google_call_back_url: "http://codial.com/users/auth/google/callback",
-  jwt_secret : 'process.env.CODIAL_JWT_SECRET'
+  jwt_secret: "codial",
+  morgan: {
+    mode: "dev",
+    options: { stream: accessLogStream },
+  },
 };
-  
-
 
 const production = {
-  name : 'production',
+  name: "production",
   asset_path: process.CODIAL_ASSET_PATH,
   session_cookie_key: "JaOIfDuIYCtaHV04KQSg0D2F8DXCkx4w",
   db: "codial_production",
@@ -37,17 +51,15 @@ const production = {
       pass: "amiabhi1708",
     },
   },
-  google_client_id:
-    "741438108020-vm71e2n8nmi68nclqen0rtuq9ssvs9bq.apps.googleusercontent.com",
-  google_client_secret: "9K6fjTc2XhxdY5JuRWW_dkru",
-  google_call_back_url: "http://codial.com/users/auth/google/callback",
-  jwt_secret : 'process.env.CODIAL_JWT_SECRET'
-}
-
+  google_client_id: "process.env.CODIAL_GOOGLE_CLIENT_ID",
+  google_client_secret: "process.CODIAL_env.GOOGLE_CLIENT_SECRET",
+  google_call_back_url: "process.CODIAL_env.GOOGLE_CALL_BACK_URL",
+  jwt_secret: "process.env.CODIAL_JWT_SECRET",
+};
 
 module.exports =
   eval(process.env.CODIAL_DEVELOPMENT) == undefined
     ? development
     : eval(process.env.CODIAL_DEVELOPMENT);
-// module.exports = 
+// module.exports =
 //to get in different passwords and static file to use it from here
--- index.js
+++ index.js
@@ -1,92 +1,93 @@
 const express = require("express");
-const env = require('./config/environment')
-const app = express();
+const env = require("./config/environment");
+const logger = require("morgan");
 const cookieParser = require("cookie-parser");
-const port = 2000;
-// setting up our layouts in
+const app = express();
+const port = 8000;
 const expressLayouts = require("express-ejs-layouts");
 const db = require("./config/mongoose");
-//used for session cookie
+// used for session cookie
 const session = require("express-session");
 const passport = require("passport");
 const passportLocal = require("./config/passport-local-strategy");
-
 const passportJWT = require("./config/passport-jwt-strategy");
-const passportGoogle = require("./config/passport-google-oauth-strategy");
+const passportGoogle = require("./config/passport-google-oauth2-strategy");
+
 const MongoStore = require("connect-mongo")(session);
 const sassMiddleware = require("node-sass-middleware");
-//const { urlencoded } = require('express');
 const flash = require("connect-flash");
+const customMware = require("./config/middleware");
 
-const customMiddleware = require("./config/middleware");
-//setup the server to be used with socket.io
+// setup the chat server to be used with socket.io
 const chatServer = require("http").Server(app);
-const chatSockets = require("./config/chat_socket").chatSockets(chatServer);
+const chatSockets = require("./config/chat_sockets").chatSockets(chatServer);
 chatServer.listen(5000);
 console.log("chat server is listening on port 5000");
-//
-const path = require('path');
-app.use(
-  sassMiddleware({
-    src:path.join(__dirname,env.asset_path,'scss'),
-    dest: path.join(__dirname,env.asset_path,'css'),
-    //do i want to display some errors the file which cant be compiled
-    debug: true,
-    outputStyle: "extended",
-    //where should my server look for css file
-    prefix: "/css",
-  })
-);
+const path = require("path");
 
-//setting up the middleware
+if (env.name == "development") {
+  app.use(
+    sassMiddleware({
+      src: path.join(__dirname, env.asset_path, "scss"),
+      dest: path.join(__dirname, env.asset_path, "css"),
+      debug: true,
+      outputStyle: "extended",
+      prefix: "/css",
+    })
+  );
+}
+
 app.use(express.urlencoded());
-//make the uploads path available to the browser
-app.use("/uploads", express.static(__dirname + "/uploads"));
 
 app.use(cookieParser());
 
-// using the static files
 app.use(express.static(env.asset_path));
-// using the layout
+// make the uploads path available to the browser
+app.use("/uploads", express.static(__dirname + "/uploads"));
+
+app.use(logger(env.morgan.mode, env.morgan.options));
+
 app.use(expressLayouts);
-// extract styles and scripts from sub pages to your layouts
+// extract style and scripts from sub pages into the layout
 app.set("layout extractStyles", true);
 app.set("layout extractScripts", true);
-// use express router
 
-//mongo store is used to store the session cookie in the db
 // set up the view engine
 app.set("view engine", "ejs");
 app.set("views", "./views");
-//add a middleware which takes the session cookie and encrypts it
+
+// mongo store is used to store the session cookie in the db
 app.use(
   session({
     name: "codial",
-    //encrypted
-    //to do change the secret before deployment to production mode that is  sending to server
-    secret: "env.session_cookie_key",
+    // TODO change the secret before deployment in production mode
+    secret: env.session_cookie_key,
     saveUninitialized: false,
+    resave: false,
     cookie: {
       maxAge: 1000 * 60 * 100,
     },
-    store: new MongoStore({
-      mongooseConnection: db,
-      autoRemove: "disabled",
-    }),
-    function(err) {
-      console.log(err || "connect-mongo setup ok");
-    },
+    store: new MongoStore(
+      {
+        mongooseConnection: db,
+        autoRemove: "disabled",
+      },
+      function (err) {
+        console.log(err || "connect-mongodb setup ok");
+      }
+    ),
   })
 );
 
 app.use(passport.initialize());
-
 app.use(passport.session());
+
 app.use(passport.setAuthenticatedUser);
 
-app.use(flash()); //flash messages are set up to cookies which stores the session information
-app.use(customMiddleware.setFlash);
-app.use(passport.isUserAuth);
+app.use(flash());
+app.use(customMware.setFlash);
+
+// use express router
 app.use("/", require("./routes"));
 
 app.listen(port, function (err) {
